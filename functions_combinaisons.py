db6138a455ae934b78bb9d705ecb979ebde524eb
ActiveZ <activez.git@gmail.com>
2021-01-13 11:41:12 +0100

    combinaisons 1 et 2 traitées

--- functions_combinaisons.py
+++ functions_combinaisons.py
@@ -1,42 +1,31 @@
 import random
 
+# -------------- TIRAGE ------------------
 
+# lancement des dés (n = nb de dés)
 def roll_dice(n):
     result = []
     for i in range(n): result.append(random.randint(1,6))
     return result
 
 
-def is_brelan(dice):
-    result = 0
-    for i in range(1,7):
-        if (dice.count(i) > 2): result = i * 3
+# ------------ COMBINAISONS 1 -------------
+
+def is_combinaison1(dice):
+    result = {
+        "dice1": dice.count(1),
+        "dice2": dice.count(2),
+        "dice3": dice.count(3),
+        "dice4": dice.count(4),
+        "dice5": dice.count(5),
+        "dice6": dice.count(6)
+    }
     return result
 
 
-def is_carre(dice):
-    result = 0
-    for i in range(1,7):
-        if (dice.count(i) > 3): result = i * 4
-    return result
+# ------------ COMBINAISONS 2 -------------
 
-
-def is_full(dice):
-    result = 0
-    b = is_brelan(dice)
-    if (b):
-        for i in range(3): dice.remove(b/3)  # enlever les 3 valeurs du brelan
-        if (dice[0] == dice[1]): result = 25
-    return result
-
-
-def is_yams(dice):
-    result = 0
-    for i in range(1,7):
-        if (dice.count(i) == 5): result = 50
-    return result
-
-
+# retourne 30 si petite suite, sinon 0
 def is_petite_suite(dice):
     ps1 = [1,2,3,4]
     ps2 = [2,3,4,5]
@@ -45,6 +34,7 @@ def is_petite_suite(dice):
     return 30 if (result) else 0
 
 
+# retourne 40 si grande suite, sinon 0
 def is_grande_suite(dice):
     gs1 = [1,2,3,4,5]
     gs2 = [2,3,4,5,6]
@@ -52,3 +42,56 @@ def is_grande_suite(dice):
     return 40 if(result) else 0
 
 
+# retourne la somme des dés du brelan, sinon 0
+def is_brelan(dice):
+    result = 0
+    for i in range(1,7):
+        if (dice.count(i) > 2): result = i * 3
+    return result
+
+
+# retourne la somme des dés du carré, sinon 0
+def is_carre(dice):
+    result = 0
+    for i in range(1,7):
+        if (dice.count(i) > 3): result = i * 4
+    return result
+
+
+# retourne 25 si full, sinon 0
+def is_full(dice):
+    tmp_dice = dice.copy()
+    result = 0
+    b = is_brelan(tmp_dice)
+    if (b):
+        for i in range(3): tmp_dice.remove(b/3)  # enlever les 3 valeurs du brelan
+        if (tmp_dice[0] == tmp_dice[1]): result = 25
+    return result
+
+
+# retourne 50 sy yams, sinon 0
+def is_yams(dice):
+    result = 0
+    for i in range(1,7):
+        if (dice.count(i) == 5): result = 50
+    return result
+
+
+# retourne la somme des dés
+def is_chance(dice):
+    return sum(dice)
+
+
+# retourne un tableaux des combinaisons réussie avec le tirage et les valeurs
+def is_combinaison2(dice):
+    result = {
+        # dice_combinaison: is_combinaison(dice),
+        "brelan": is_brelan(dice),
+        "carre": is_carre(dice),
+        "full": is_full(dice),
+        "petite_suite": is_petite_suite(dice),
+        "grande_suite": is_grande_suite(dice),
+        "yams": is_yams(dice),
+        "chance": is_chance(dice)
+    }
+    return result
--- launch.py
+++ launch.py
@@ -1,11 +1,28 @@
+import os
 from functions_combinaisons import *
 
 
-print("tirage: ", roll_dice(5))
+os.system('cls')  # attention, sur ubuntu, remplacer par os.system('clear')
 
-print("petite suite: ", is_petite_suite([6,2,3,4,4,5]))
-print("grande suite: ", is_grande_suite([1,3,2,4,5]))
-print("brelan: ", is_brelan([6,6,6,6,4]))
-print("carre: ", is_carre([1,1,1,1,4]))
-print("full: ", is_full([3,3,2,2,2]))
-print("yams: ", is_yams([2,2,2,2,2]))
+
+tirage = roll_dice(5)
+print("tirage: ", tirage)
+
+# print("petite suite: ", is_petite_suite([6,2,3,4,4,5]))
+# print("grande suite: ", is_grande_suite([1,3,2,4,5]))
+# print("brelan: ", is_brelan([6,6,6,6,4]))
+# print("carre: ", is_carre([1,1,1,1,4]))
+# print("full: ", is_full([3,3,2,2,2]))
+# print("yams: ", is_yams([2,2,2,2,2]))
+# print("chance: ", is_chance([2,2,2,2,2]))
+
+print("petite suite: ", is_petite_suite(tirage))
+print("grande suite: ", is_grande_suite(tirage))
+print("brelan: ", is_brelan(tirage))
+print("carre: ", is_carre(tirage))
+print("full: ", is_full(tirage))
+print("yams: ", is_yams(tirage))
+print("chance: ", is_chance(tirage))
+
+print("combinaisons 1:", is_combinaison1(tirage))
+print("combinaisons 2:", is_combinaison2(tirage))
