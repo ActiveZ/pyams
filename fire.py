859f51cecca226a4942795c807b8082f8336d9ea
ActiveZ <activez.git@gmail.com>
2021-01-15 01:43:13 +0100

    crud firebase ok

--- fire.py
+++ fire.py
@@ -9,50 +9,69 @@ firebase = firebase.FirebaseApplication(URL, None)
 def create_game(nb_players):
     data = {}
     for i in range(nb_players):
-        data["j"+str(i+1)] = player_db
+        data["j"+str(i+1)] = player_db.copy()
     # print("data", data)
-    result = firebase.post("/pyams/scores", data)
+    result = firebase.post("/pyams/", data)
     # print("firebase:", result)
     print (result["name"]," game created with",nb_players,"players !\n")
     return result["name"]
 
 
-def read_game():
-    result = firebase.get("/pyams/scores/", "")
-    print(result,'\n')
-    print("nb games:", len(result),'\n')
-    print("Game list:", list(result),'\n')
-    # print("db 1:",list(result)[0], '\n')
+def get_list_games():
+    result = firebase.get("/pyams/", "")
+    # print(result,'\n')
+    # print("nb games:", len(result),'\n')
+    # print("Game list:", list(result),'\n')
+    return list(result)
+
+
+def read_game(game_id):
+    result = firebase.get("/pyams/" + game_id, "")
+    print("Current game:", result,'\n')
 
 
 def update_game(game_id, data):
-    data = {
-        "name": "joueur 2",
-        "dice2" : 6,
-        "yams": True
-        }
-    firebase.put("/pyams/scores/" + game_id,"dice1",666)
-    print(game_id,"Updated\n")
+    for k, v in data.items(): # update key, value for all players in data
+        firebase.put("/pyams/" + game_id, k, v)
+    print(game_id,"updated\n")
 
 
 def delete_game(game_id):
-    firebase.delete("/pyams/scores/", game_id)
-    # firebase.delete("/pyams/scores/", "-MR0xxC5lBs9px_jLPvs")
-    print(game_id,"Game Deleted\n")
+    firebase.delete("/pyams/", game_id)
+    print(game_id,"game deleted\n")
 
 
 def delete_all_games():
-    result = firebase.get("/pyams/scores", "")
-    if (result == None): return
+    i = 0
+    result = firebase.get("/pyams/", "")
+    if (result == None): 
+        print("No game to delete")
+        return
     for db in list(result):
-        firebase.delete("/pyams/scores/", db)
-        print(db,"deleted !\n")
+        firebase.delete("/pyams/", db)
+        print(db,"deleted !")
+        i += 1
+    print(i, "game(s) deleted !\n")
 
 
 os.system('clear')  # attention, sur ubuntu, remplacer par os.system('clear')
-delete_all_games()
-create_game(6)
-# read_game()
-# update_game()
-# read_game()
-# delete_game()
+
+delete_all_games() # efface tous les jeux de la db
+create_game(6) # crée un jeu à 6 joueurs
+game_list = get_list_games() # récupére la liste des jeux de la db
+current_game = game_list[len(game_list) - 1] # le jeu en cours est le dernier de la liste
+print("current game:", current_game)
+read_game(current_game) # affichage des données du jeu en cours
+
+# nouvelles données
+data = {}
+j1 = player_db.copy()
+j1["name"] = "joueur 1"
+data["j1"] = j1
+j2 = player_db.copy()
+j2["name"] = "joueur 2"
+data["j2"] = j2
+update_game(current_game, data) # update du jeu en cours
+
+read_game(current_game) # vérification de l'update
+# delete_game(current_game) # effacement du jeu en cours
